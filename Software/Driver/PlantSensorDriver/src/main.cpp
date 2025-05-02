bf18972a86763d0a067953263cc1d26d5ebe5dc0
agerber0902 <agerber0902@gmail.com>
2025-05-02 16:35:12 -0400

    add empty callback function

--- Software/Driver/PlantSensorDriver/src/main.cpp
+++ Software/Driver/PlantSensorDriver/src/main.cpp
@@ -2,14 +2,21 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-
-void setup() {
+void setup()
+{
   Serial.begin(921600);
 
   // Initialize ESP-NOW
   WiFi.mode(WIFI_STA);
   esp_now_init();
+
+  // Callback function for receiving data
+  esp_now_register_recv_cb([](const uint8_t *mac, const uint8_t *data, int len)
+  {
+    
+  });
 }
 
-void loop() {
+void loop()
+{
 }
